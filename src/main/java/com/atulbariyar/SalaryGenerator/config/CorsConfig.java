3a4bd09ac1fc861ad8efb4ad8fd7a99dad582b82
Atul <atulbariyar511@gmail.com>
2025-11-05 20:48:42 +0530

    Cors Config

---
+++ src/main/java/com/atulbariyar/SalaryGenerator/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.atulbariyar.SalaryGenerator.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://salarydashboard.vercel.app" // your frontend URL
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
